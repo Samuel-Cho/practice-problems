f9e546368fd192df38214b68065d56b36ae21f09
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-03-15 01:11:19 +0000

    Add solution to reverseInParentheses from CodeSignal.

--- CodeSignal/reverseInParenthesis.js
+++ CodeSignal/reverseInParenthesis.js
@@ -1,45 +1,22 @@
 function reverseInParentheses(inputString) {
-    let reverseString = '';
     let numParentheses = 0;
+    let reverseString = '';
     let returnString = inputString;
-    let layeredParentheses = false;
-    let layeredOpen = 0;
     for (let i = 0; i < inputString.length; i++) {
         if (inputString[i] === '(') {
             numParentheses++;
-            layeredOpen++;
-            if (layeredOpen > 1) {
-                layeredParentheses = true;
-            } else {
-                layeredParentheses = false;
-            }
-        }
-        if (inputString[i] === ')') {
-            layeredOpen--;
         }
     }
-    for (let k = numParentheses; k > 0; k--) {
-        if (layeredParentheses) {
-            let openParenthesesIndex = returnString.lastIndexOf('(');
-            let closeParenthesesIndex = returnString.indexOf(')');
-            for (let j = closeParenthesesIndex - 1; j > openParenthesesIndex; j--) {
-                reverseString += returnString[j];
-            }
-            let stringPart1 = returnString.slice(0, openParenthesesIndex);
-            let stringPart2 = returnString.slice(closeParenthesesIndex + 1);
-            returnString =  stringPart1 + reverseString + stringPart2;
-            reverseString = '';
-        } else {
-            let openParenthesesIndex = returnString.indexOf('(');
-            let closeParenthesesIndex = returnString.indexOf(')');
-            for (let j = closeParenthesesIndex - 1; j > openParenthesesIndex; j--) {
-                reverseString += returnString[j];
-            }
-            let stringPart1 = returnString.slice(0, openParenthesesIndex);
-            let stringPart2 = returnString.slice(closeParenthesesIndex + 1);
-            returnString =  stringPart1 + reverseString + stringPart2;
-            reverseString = '';
+    for (let j = 0; j < numParentheses; j++) {
+        let innerOpenParentheses = returnString.lastIndexOf('(');
+        let innerCloseParentheses = returnString.indexOf(')', innerOpenParentheses)
+        for (let k = innerCloseParentheses - 1; k > innerOpenParentheses; k--) {
+            reverseString += returnString[k];
         }
+        let stringPart1 = returnString.slice(0, innerOpenParentheses);
+        let stringPart2 = returnString.slice(innerCloseParentheses + 1);
+        returnString = stringPart1 + reverseString + stringPart2;
+        reverseString = '';
     }
-    return returnString
+    return returnString;
 }
